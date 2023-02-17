a552fbe11bacaf5922a6d2f40667e25601552707
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-02-17 11:59:39 +0530

    17 Feb 2023

---
+++ DSA/Lecture 19 - Basic Math For DSA/gcd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    else if (b == 0)
+        return a;
+
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+
+int main()
+{
+
+    int a, b;
+    cout << "Enter value A and value B" << endl;
+    cin >> a >> b;
+    int ans = gcd(a, b);
+    cout << "The GCD of " << a << " & " << b << " is :" << ans << endl;
+    return 0;
+}
