c55e0ddb80996fcda73f01f2de60eea88f6dd37f
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-02-22 12:45:52 +0530

    22 Feb 2023

---
+++ DSA/Lecture 22 - Pointers in Cpp part 3/doublePointer.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int val = 10;
+    int *p = &val; // Single pointer initialization
+    int **p2 = &p; // Two pointers initialization
+
+    cout << endl;
+    // Printing the pointers
+
+    cout << "Printing val : " << val << endl;   // output 10 all will print the same value
+    cout << "Printing &val : " << &val << endl; // output will be address of val
+
+    cout << endl;
+
+    cout << "Printing p : " << p << endl;   // output will be address of val
+    cout << "Printing *p : " << *p << endl; // output 10 all will print the same value
+    cout << "Printing &p : " << &p << endl; // output will be address of first pointer
+
+    cout << endl;
+
+    cout << "Printing p2 : " << p2 << endl;     // output will be address of first pointer
+    cout << "Printing *p2 : " << *p2 << endl;   // output will be address of val
+    cout << "Printing **p2 : " << **p2 << endl; // output 10 all will print the same value
+    cout << "Printing &p2 : " << &p2 << endl; 
+    cout << endl;
+
+    return 0;
+}
